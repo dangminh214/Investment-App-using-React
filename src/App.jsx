29bcb1e4bf7401a89f8533579a52c15556efe7d4
dangminh214 <dangminh214@gmail.com>
2024-03-23 19:54:06 +0100

    fix bug that userInput doenst work

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import Results from './components/Results.jsx';
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
+    initialInvestment: 10000,
     annualInvestment: 1200, 
     expectedReturn: 6,
     duration: 10, 
   });
+
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
@@ -25,7 +27,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput = {userInput} onChange = {handleChange}/>
-      <Results input = {userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero</p>}
+      { inputIsValid && <Results input = {userInput}/>}
     </>
   )
 }
